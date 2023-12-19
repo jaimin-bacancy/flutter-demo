bd89876ba8b4df0177574adb818dca221306b536
Jaimin Bhut <jaimin.bhut@bacancy.com>
2023-12-19 18:12:09 +0530

    add env file

--- lib/app/services/google_auth_service.dart
+++ lib/app/services/google_auth_service.dart
@@ -1,16 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleAuthService {
   signInWithGoogle() async {
     final GoogleSignInAccount? gUser = await GoogleSignIn(
-      clientId:
-          '966535779882-u0jpg4pqnfbj09rap788v5fq8u1gkmdb.apps.googleusercontent.com',
+      clientId: dotenv.env['GOOGLE_CLIENT_ID'],
     ).signIn();
 
     GoogleSignInAuthentication gAuth = await gUser!.authentication;
-
-    // final credential = GoogleAuthProvider.credential(
-    //     accessToken: gAuth.accessToken, idToken: gAuth.idToken);
 
     return gAuth;
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:awesome_app/app/presentation/screens/startup/startup_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   await GetStorage.init();
   runApp(const ProviderScope(child: App()));
